c223d4073d352a01f7727bcc4236f532ecd6f1c8
NuoZai-github <lime6199@gmail.com>
2025-12-08 00:38:10 +0800

    Fix puppeteer args for Docker

--- index.js
+++ index.js
@@ -56,7 +56,16 @@ const model = genAI.getGenerativeModel({
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
     }
 });
 
